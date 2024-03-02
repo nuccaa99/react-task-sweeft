f9ba60130333f7d2f52801dcf97e6dc0aa565c17
Nino (Nutsa) Kharazishvili <nutsa1999@gmail.com>
2024-03-02 20:44:32 +0400

    minor changes

--- src/App.js
+++ src/App.js
@@ -1,59 +1,21 @@
-import React, { useEffect, useState } from 'react';
 //styles
 import "./styles/reset.css";
 import "./styles/style.css";
 //routing
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-//components
+//components & pages
 import Home from './pages/Home';
 import History from './pages/History';
 import Navbar from './components/Navbar';
-import API from "./API";
+
 
 function App() {
-  const initialState = "most popular";
-  const [term, setTerm] = useState(initialState);
-  const [imageList, setImageList] = useState([]);
-
-  const fetchImages = async () => {
-    try {
-      const data = await API.fetchImages(term);
-      setImageList(data.results)
-      if (term) {
-        const existingStorage = JSON.parse(localStorage.getItem("imagesData")) || [];
-        const newSearchResult = { id: term, data: data.results }
-        const updatedStorage = [...existingStorage, newSearchResult]
-        localStorage.setItem("imagesData", JSON.stringify(updatedStorage));
-      }
-    } catch (error) {
-      console.log(error);
-    }
-  };
-
-  useEffect(() => {
-    if (term.length === 0) {
-      setTerm(initialState)
-    } else {
-      const existingStorage = JSON.parse(localStorage.getItem("imagesData")) || [];
-      const cachedData = existingStorage.find((item) => item.id === term)?.data;
-      if (cachedData) {
-        // console.log("local")
-        setImageList(cachedData);
-
-      } else {
-        // console.log("api")
-        fetchImages()
-      }
-    }
-
-  }, [term]);
-
-
+  
   return (
     <Router>
       <Navbar />
       <Routes>
-        <Route path='/' element={<Home setTerm={setTerm} imageList={imageList} />} />
+        <Route path='/' element={<Home />} />
         <Route path='/history' element={<History />} />
       </Routes>
     </Router >
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,51 @@
+import React, { useEffect, useState } from 'react';
+
+
 import ImageList from "../components/ImageList";
 import SearchBar from "../components/SearchBar"
 
+import API from "../API";
 
-function Home({ imageList, setTerm }) {
+
+function Home() {
+
+    const initialState = "most popular";
+    const [term, setTerm] = useState(initialState);
+    const [imageList, setImageList] = useState([]);
+  
+    const fetchImages = async () => {
+      try {
+        const data = await API.fetchImages(term);
+        setImageList(data.results)
+        if (term) {
+          const existingStorage = JSON.parse(localStorage.getItem("imagesData")) || [];
+          const newSearchResult = { id: term, data: data.results }
+          const updatedStorage = [...existingStorage, newSearchResult]
+          localStorage.setItem("imagesData", JSON.stringify(updatedStorage));
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    };
+  
+    useEffect(() => {
+      if (term.length === 0) {
+        setTerm(initialState)
+      } else {
+        const existingStorage = JSON.parse(localStorage.getItem("imagesData")) || [];
+        const cachedData = existingStorage.find((item) => item.id === term)?.data;
+        if (cachedData) {
+          // console.log("local")
+          setImageList(cachedData);
+  
+        } else {
+          // console.log("api")
+          fetchImages()
+        }
+      }
+  
+    }, [term]);
+
     return (
         <div className="home_container">
             <SearchBar setTerm={setTerm} />
